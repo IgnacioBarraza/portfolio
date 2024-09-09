2afddcfc9ee30016b0fb5a98a776bfe6b47b9d81
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2024-09-08 22:39:48 -0300

    🐛 github link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -41,7 +41,7 @@ export default function About() {
                 Contact me
               </Link>
               <Link
-                href={"https://www.linkedin.com/in/ignacio-barraza-rioja/"}
+                href={"https://github.com/IgnacioBarraza"}
                 className="inline-flex h-9 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                 prefetch={false}
                 target="blank"
