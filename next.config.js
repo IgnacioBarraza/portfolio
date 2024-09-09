0bac50550c64c6ea4df138ed072461257f1ca42d
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2024-09-09 10:50:38 -0300

    ⚗️ experimenting with swc disabled

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  swcMinify: false,
+  experimental: {
+    appDir: true
+  }
+}
 
 module.exports = nextConfig
